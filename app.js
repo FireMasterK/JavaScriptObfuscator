ebf27dd3b424fae048dcb3f2609a243c8711c376
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2020-03-29 00:30:08 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+var JavaScriptObfuscator = require('javascript-obfuscator');
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("JavaScript Obfuscator by Kavin");
+});
+
+app.post('/obfuscate', (req, res) => {
+    var obfuscationResult = JavaScriptObfuscator.obfuscate(
+        req.body.code,
+        {
+            compact: true,
+            controlFlowFlattening: true,
+            controlFlowFlatteningThreshold: 1.0,
+            deadCodeInjection: false,
+            deadCodeInjectionThreshold: 0.4,
+            debugProtection: true,
+            debugProtectionInterval: true,
+            disableConsoleOutput: true,
+            domainLock: req.body.domains,
+            identifierNamesGenerator: 'mangled',
+            identifiersDictionary: [],
+            identifiersPrefix: '',
+            inputFileName: '',
+            log: false,
+            renameGlobals: false,
+            reservedNames: [],
+            reservedStrings: [],
+            rotateStringArray: true,
+            selfDefending: true,
+            shuffleStringArray: true,
+            sourceMap: false,
+            sourceMapBaseUrl: '',
+            sourceMapFileName: '',
+            sourceMapMode: 'separate',
+            splitStrings: true,
+            splitStringsChunkLength: 5,
+            stringArray: true,
+            stringArrayEncoding: 'rc4',
+            stringArrayThreshold: 0.75,
+            target: 'browser-no-eval',
+            transformObjectKeys: true,
+            unicodeEscapeSequence: false
+        }
+    );
+
+    var code = obfuscationResult.obfuscatedCode;
+
+    res.send(code);
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
