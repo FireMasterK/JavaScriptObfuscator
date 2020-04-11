f599f01f56a05574c169e1c5111c7ecabe92642d
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2020-04-11 23:17:55 +0530

    Only listen on localhost.

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ var app = require('./app.js');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`JavaScript Obfuscator listening on port ${port}!`))
+app.listen(port, 'localhost', () => console.log(`JavaScript Obfuscator listening on port ${port}!`))
